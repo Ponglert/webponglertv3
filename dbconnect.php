d29703c1d4ebaa97c3f10832b40a6de28772a294
Ponglert <asakuratran@gmail.com>
2026-02-17 15:39:16 +0700

    upv2

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
     $host = "localhost";
-    $username = "ponglert"; //UserName ที่ใช้ login ใน phpMyAdmin
-    $password = "masterx0"; //Password ที่ใช้ login ใน phpMyAdmin
-    $dbname = "ponglert_myshop"; //ชื่อฐานข้อมูลที่เราสร้าง
+    $username = "root"; //UserName ที่ใช้ login ใน phpMyAdmin
+    $password = ""; //Password ที่ใช้ login ใน phpMyAdmin
+    $dbname = "ponglertv2"; //ชื่อฐานข้อมูลที่เราสร้าง
     try{
         $con = new PDO('mysql:host='.$host.';dbname='.$dbname,$username,$password);
         $con ->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
--- mget.php
+++ mget.php
@@ -1,10 +1,15 @@
 <?php
     include'dbconnect.php';
     if(isset($_GET['id_user'])){
-        $stmt=$con->prepare("SELECT * from members");
-        $stmt->execute();
-        $results = $stmt->fetchAll();
-        echo json_encode($results);
+	if($_GET['id_user'] == '123456'){
+           $stmt=$con->prepare("SELECT * from members");
+           $stmt->execute();
+           $results = $stmt->fetchAll();
+           echo json_encode($results);
+	}else{
+	   $response["id"] = 0;
+           echo json_encode($response);
+	}
     }else{
         $response["id"] = 0;
         echo json_encode($response);
